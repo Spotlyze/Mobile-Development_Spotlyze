0d6bf420bd03c46260b3b3f017d0b64efc368fe7
prayatnaaa <tudeprayatna04@gmail.com>
2024-11-13 02:01:22 +0800

    add user settings

--- app/src/main/java/com/bangkit/spotlyze/ui/home/HomeFragment.kt
+++ app/src/main/java/com/bangkit/spotlyze/ui/home/HomeFragment.kt
@@ -1,19 +1,39 @@
 package com.bangkit.spotlyze.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.prayatna.spotlyze.R
+import com.bangkit.spotlyze.ui.user_settings.UserSettingsActivity
+import com.prayatna.spotlyze.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.mToolBar.toolBarProfile.setOnClickListener {
+            val intent = Intent(requireActivity(), UserSettingsActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
