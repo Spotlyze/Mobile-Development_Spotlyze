20cdb82bf47baefc225f9a15bb8c14920e295463
prayatnaaa <tudeprayatna04@gmail.com>
2024-12-05 16:10:54 +0800

    fix bug: arrange diffCallback to not fully refresh when state changes

--- app/src/main/java/com/bangkit/spotlyze/helper/DiffCallback.kt
+++ app/src/main/java/com/bangkit/spotlyze/helper/DiffCallback.kt
@@ -2,12 +2,11 @@ package com.bangkit.spotlyze.helper
 
 import androidx.recyclerview.widget.DiffUtil
 import com.bangkit.spotlyze.data.local.database.entity.SkincareEntity
-import com.bangkit.spotlyze.data.remote.response.GetSkincareResponseItem
 
 internal object DiffCallback : DiffUtil.ItemCallback<SkincareEntity>() {
 
     override fun areItemsTheSame(oldItem: SkincareEntity, newItem: SkincareEntity): Boolean {
-        return oldItem == newItem
+        return oldItem.skincareId == newItem.skincareId
     }
 
     override fun areContentsTheSame(oldItem: SkincareEntity, newItem: SkincareEntity): Boolean {
